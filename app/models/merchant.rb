75f9d9cfc45ccb5509d8136e89a37a781c7a3454
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-27 13:22:31 -0600

    adds customer, transaction, and merchant validation specs

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
+  validates :name, presence: true
+  
   has_many :invoices
   has_many :customers, through: :invoices
   has_many :items
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Transaction do
+  context "validations" do
+    it { should validate_presence_of(:invoice_id)}
+    it { should validate_presence_of(:credit_card_number)}
+    it { should validate_presence_of(:result)}
+  end
 end

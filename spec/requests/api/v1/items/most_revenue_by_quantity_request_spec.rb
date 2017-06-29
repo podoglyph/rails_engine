7380b4c13eebe9dd9d8aef51fa1551ae8ed46251
frustratedswede <andrew.mossefin@gmail.com>
2017-06-29 12:40:32 -0600

    fix merchant spec

--- spec/requests/api/v1/items/most_revenue_by_quantity_request_spec.rb
+++ spec/requests/api/v1/items/most_revenue_by_quantity_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-description "GET /api/v1/items/most_revenue?quantity=x" do
+describe "GET /api/v1/items/most_revenue?quantity=x" do
   it "returns the top x items ranked by total revenue generated" do
 
   end

68988de55169bad13bfed67e1db41bba059bd0dd
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-29 16:15:05 -0600

    adds items_best_day

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,7 +1,7 @@
-class Api::V1::Items::BestDayController < ApplicationRecord
+class Api::V1::Items::BestDayController < ApplicationController
 
   def show
-    
+    render json: Item.find(params["id"]).best_day
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,15 @@ class Item < ApplicationRecord
     .order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
     .limit(quantity)
   end
+
+  def best_day
+    { "best_day" => invoices
+      .joins(:invoice_items)
+      .group(:id)
+      .group(:created_at)
+      .order("sum(invoice_items.quantity) DESC")
+      .first
+      .created_at }
+  end
+
 end
